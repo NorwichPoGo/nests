2ff7d641997a28462b3da95ac57c6ba2fe6decbc
arandomoracle <5637338+arandomoracle@users.noreply.github.com>
2018-04-19 03:11:29 +0200

    Moved settings into their own script.

--- assets/js/gyms.js
+++ assets/js/gyms.js
@@ -1,11 +1,5 @@
 const featuresFile = '/data/features.json';
 
-const featureMarkerIcons = {
-  gym: '/assets/images/gym.png',
-  pokestop: '/assets/images/pokestop.png',
-  portal: '/assets/images/portal.png'
-};
-
 /**
  * Modified from https://stackoverflow.com/questions/19491336 .
  */
@@ -27,90 +21,6 @@ function urlParameter(parameterName) {
   });
 
   return parameterValue;
-};
-
-const Settings = {
-  settings: {
-    showGyms: {
-      type: 'boolean',
-      defaultValue: true
-    },
-    showPokestops: {
-      type: 'boolean',
-      defaultValue: false
-    },
-    showPortals: {
-      type: 'boolean',
-      defaultValue: false
-    },
-    s2Cells: {
-      type: 'json',
-      defaultValue: []
-    },
-    s2CellColors: {
-      type: 'json',
-      defaultValue: {
-        '1': '#FF6F00',
-        '2': '#F57F17',
-        '3': '#827717',
-        '4': '#33691E',
-        '5': '#1B5E20',
-        '6': '#004D40',
-        '7': '#006064',
-        '8': '#01579B',
-        '9': '#0D47A1',
-        '10': '#1A237E',
-        '11': '#6A1B9A',
-        '12': '#AD1457',
-        '13': '#b71c1c',
-        '14': '#BF360C',
-        '15': '#FF7043',
-        '16': '#FFCA28',
-        '17': '#FDD835',
-        '18': '#00796B',
-        '19': '#0288D1',
-        '20': '#64B5F6'
-      }
-    },
-    mapCenter: {
-      type: 'json',
-      defaultValue: {
-        lat: 52.63282,
-        lng: 1.29732
-      }
-    },
-    zoomLevel: {
-      type: 'float',
-      defaultValue: 13
-    }
-  },
-  get: function(settingName) {
-    const setting = Settings.settings[settingName];
-    const rawSettingValue = localStorage.getItem(settingName);
-
-    if (rawSettingValue === undefined || rawSettingValue === null) {
-      return setting.defaultValue;
-    }
-
-    if (setting.type == 'boolean') {
-      return rawSettingValue == 'true';
-    } else if (setting.type == 'float') {
-      return parseFloat(rawSettingValue);
-    } else if (setting.type == 'json') {
-      return JSON.parse(rawSettingValue);
-    }
-
-    return rawSettingValue;
-  },
-  set: function (settingName, value) {
-    const setting = Settings.settings[settingName];
-
-    if (setting.type == 'json') {
-      localStorage.setItem(settingName, JSON.stringify(value));
-    } else {
-      localStorage.setItem(settingName, value);
-    }
-  }
 };
 
 function initMap() {
@@ -373,6 +283,11 @@ function zoomToFeature(map, feature, zoom) {
 }
 
 function drawFeature(map, feature) {
+  const featureMarkerIcons = {
+    gym: '/assets/images/gym.png',
+    pokestop: '/assets/images/pokestop.png',
+    portal: '/assets/images/portal.png'
+  };
   const anchorX = (feature.type == 'gym') ? 22 : 15;
 
   let featureMarker;
