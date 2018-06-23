169c96805fd144aa1a7e917b47e7db8ec3f5e3f2
arandomoracle <5637338+arandomoracle@users.noreply.github.com>
2018-06-23 19:28:40 +0200

    Added a pre-push hook to rebuild assets automatically.

---
+++ bin/pre_push.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const git = require('simple-git/promise')();
+const grunt = require('grunt');
+const Gruntfile = require('../Gruntfile')(grunt);
+
+const isSourceFile = filename => {
+  const isGruntfile = filename == 'Gruntfile.js';
+  const isSourceFile = filename.match(/^src\/js/);
+  return isGruntfile || isSourceFile;
+};
+
+const checkForSourceChanges = () => {
+  return git.diffSummary('origin/master', 'master')
+    .then(diff => {
+      return diff.files.find(change => isSourceFile(change.file));
+    });
+};
+
+const checkForStagedFiles = () => {
+  return git.status()
+    .then(status => {
+      return status.staged.length > 0;
+    });
+};
+
+const checkForUncommittedChanges = () => {
+  return git.status()
+    .then(status => {
+      return status.files.find(file => isSourceFile(file.path));
+    });
+};
+
+const buildCommitAndPushAssets = () => {
+  grunt.tasks('build');
+
+  return git.add('assets/js')
+    .then(() => {
+      return git.commit('Automatic asset rebuild.');
+    })
+    .then(() => {
+      return git.push('origin', 'master');
+    });
+};
+
+checkForSourceChanges()
+  .then(sourceChanged => {
+    if (!sourceChanged) return;
+
+    return checkForStagedFiles()
+      .then(stagedFiles => {
+        if (stagedFiles) {
+          throw new Error(
+            'Can\'t perform an automatic build. ' +
+            'Please commit or stash any staged files.'
+          );
+        }
+      })
+      .then(() => {
+        return checkForUncommittedChanges();
+      })
+      .then(uncommittedChanges => {
+        if (uncommittedChanges) {
+          throw new Error(
+            'Source files have been modified since the last commit. ' +
+            'Please commit or stash any modified source files.'
+          );
+        }
+      })
+      .then(() => {
+        return buildCommitAndPushAssets();
+      });
+  })
+  .catch(error => {
+    console.log(error);
+    process.exit(1);
+  });
