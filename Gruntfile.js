733ed43eb49237162a0123d377b9dc47e4ec9605
arandomoracle <5637338+arandomoracle@users.noreply.github.com>
2018-07-08 18:58:23 +0200

    Added a task for favicon generation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,6 +34,85 @@ module.exports = grunt => {
     },
     clean: {
       build: 'assets/js'
+    },
+    realFavicon: {
+      favicons: {
+        src: 'assets/images/icons/favicon_n_source.png',
+        dest: 'assets/images/icons',
+        options: {
+          iconsPath: '/assets/images/icons',
+          html: '_includes/favicon.html',
+          design: {
+            ios: {
+              masterPicture: {
+                type: 'inline',
+                content: 'assets/images/icons/favicon_source.png'
+              },
+              pictureAspect: 'noChange',
+              assets: {
+                ios6AndPriorIcons: false,
+                ios7AndLaterIcons: false,
+                precomposedIcons: false,
+                declareOnlyDefaultIcon: true
+              },
+              appName: 'Pokémon Go Norwich'
+            },
+            desktopBrowser: {
+              masterPicture: {
+                type: 'inline',
+                content: 'assets/images/icons/favicon_source.png'
+              }
+            },
+            windows: {
+              pictureAspect: 'noChange',
+              backgroundColor: '#603cba',
+              onConflict: 'override',
+              assets: {
+                windows80Ie10Tile: false,
+                windows10Ie11EdgeTiles: {
+                  small: false,
+                  medium: true,
+                  big: false,
+                  rectangle: false
+                }
+              },
+              appName: 'Pokémon Go Norwich'
+            },
+            androidChrome: {
+              pictureAspect: 'noChange',
+              themeColor: '#603cba',
+              manifest: {
+                name: 'Pokémon Go Norwich',
+                display: 'standalone',
+                orientation: 'notSet',
+                onConflict: 'override',
+                declared: true
+              },
+              assets: {
+                legacyIcon: true,
+                lowResolutionIcons: false
+              }
+            },
+            safariPinnedTab: {
+              pictureAspect: 'blackAndWhite',
+              threshold: 64.6875,
+              themeColor: '#603cba'
+            }
+          },
+          settings: {
+            compression: 2,
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
+          },
+          versioning: {
+            paramName: 'v',
+            paramValue: '3'
+          }
+        }
+      }
     }
   });
 
@@ -41,9 +120,11 @@ module.exports = grunt => {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-real-favicon');
 
   grunt.registerTask('default', 'build');
   grunt.registerTask('lint', 'eslint');
   grunt.registerTask('build', ['clean', 'webpack:build', 'copy']);
   grunt.registerTask('watch', 'webpack:watch');
+  grunt.registerTask('favicons', 'realFavicon');
 };
